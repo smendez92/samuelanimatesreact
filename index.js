4d84e7d720787082616995ba5a909339d7d849af
smendez92 <samuelrmendez14@gmail.com>
2018-08-21 14:31:27 -0500

    server fix again?

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port);
